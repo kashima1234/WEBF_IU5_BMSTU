dff638213e608ea8ac00b44f722189a789afb30a
kashimaa <kashimaahmed@gmail.com>
2024-11-26 22:28:42 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/WEBF_IU5_BMSTU/sw.js', { scope: '/WEBF_IU5_BMSTU/' })})}
