4cf17c07fb1d29a2597897cda4ee9c61abf3ed1d
kashimaa <kashimaahmed@gmail.com>
2025-01-16 17:19:05 +0300

    fixing

--- VikingsServer/vite.config.ts
+++ VikingsServer/vite.config.ts
@@ -1,30 +1,3 @@
-import {defineConfig, loadEnv} from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-
-export default ({mode}:{mode:string}) => {
-    process.env = loadEnv(mode, process.cwd());
-
-    return defineConfig({
-        base: "/WEBF_IU5_BMSTU",
-        server: {
-            host: true,
-            port: 3000,
-            proxy: {
-                "/api": {
-                    target: process.env.VITE_API_URL
-                }
-            },
-        },
-        plugins: [
-            react(),
-            tsconfigPaths()
-        ]
-    })
-}
-
-
-
 // import {defineConfig, loadEnv} from 'vite'
 // import react from '@vitejs/plugin-react'
 // import tsconfigPaths from 'vite-tsconfig-paths'
@@ -39,7 +12,7 @@ export default ({mode}:{mode:string}) => {
 //             port: 3000,
 //             proxy: {
 //                 "/api": {
-//                     target: "http://172.16.87.217:8000"
+//                     target: process.env.VITE_API_URL
 //                 }
 //             },
 //         },
@@ -49,4 +22,31 @@ export default ({mode}:{mode:string}) => {
 //         ]
 //     })
 // }
+
+
+
+import {defineConfig, loadEnv} from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default ({mode}:{mode:string}) => {
+    process.env = loadEnv(mode, process.cwd());
+
+    return defineConfig({
+        base: "/WEBF_IU5_BMSTU",
+        server: {
+            host: true,
+            port: 3000,
+            proxy: {
+                "/api": {
+                    target: "http://192.168.23.47:8000"
+                }
+            },
+        },
+        plugins: [
+            react(),
+            tsconfigPaths()
+        ]
+    })
+}
 
---
+++ fixup/README.md
@@ -0,0 +1,38 @@
+- [X] [НЕ НАДО САМОМУ ДЕЛАТЬ ПОРЯДОК, ТЫ ПРОСТО НАЖИМАЕШЬ КНОПКУ ВНИЗ и ОН САМ СЛЕДУЮЩУЮ С ПРЕДЫДУЩЕЙ.
+ДОЛЖНА БЫТЬ СОРТИРОВКА "PUT м-м. Поменять порядок (down) для верхней услуги в заявке: было 1-2-3, стало 2-1-3](https://github.com/kashima1234/WEBF_IU5_BMSTU/tree/frontend/SPA)
+
+![1](1.png)
+
+- [x] [](https://github.com/kashima1234/WEBF_IU5_BMSTU/blob/Real-time/VikingsServer/src/components/PlaceCard/PlaceCard.tsx)
+ 
+2 - ПОЧЕМУ НЕ РАБОТАЕТ ФОРМИРОВАНИЕ? ЗАЯВКА Должать стать сформированной и перейти в список заявок
+<img src="v1.gif" />
+
+https://github.com/kashima1234/WEBF_IU5_BMSTU/blob/Real-time/VikingsServer/src/pages/ExpeditionPage/ExpeditionPage.tsx
+
+
+
+4- прислать в коде использование thunk, где ты их используешь?
+
+![2](2.png)
+
+https://github.com/kashima1234/WEBF_IU5_BMSTU/blob/Real-time/VikingsServer/src/pages/ExpeditionPage/ExpeditionPage.tsx
+
+https://github.com/kashima1234/WEBF_IU5_BMSTU/blob/Real-time/VikingsServer/src/store/slices/expeditionsSlice.ts
+
+5- - не работает завершении и Отклонение заявки
+<img src="v2.gif" />
+
+6 - список заявок: убрать дату созданеия, и дату завершения, но доавить поле Кто возглавлял
+
+дату созданеия, и дату завершения has been deleted and been add возглавлял as well as 
+<img src="v2.gif" />
+
+
+7-  - таблице услуг нужны маленькие картинки. Кнопка удалить должна быть только в общей таблице
+![3](3.png)
+
+https://github.com/kashima1234/WEBF_IU5_BMSTU/blob/Real-time/VikingsServer/src/components/PlacesTable/PlacesTable.tsx
+
+8 - показать содержимое редис в командной строке компьютера, а не в docker
+![3](4.png)
